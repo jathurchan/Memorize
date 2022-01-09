4edffa4f71dcc2ef3fed66fdbcf737f02bf33a88
Jathurchan Selvakumar <mail@jathurchan.com>
2022-01-09 19:17:23 +0100

    Created the project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jathurchan Selvakumar on 09/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
